a493288b4815d038a7c43c1687da5b323ef52c35
DrittesPhantom <DrittesPhantom@users.noreply.github.com>
2026-07-12 14:18:38 +0000

    deploy: DrittesPhantom/BlazorWebAppTemplate@bf49490df3c6bc343cc63356d76bde40e71d3b76

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: YWt969fY */
+/* Manifest version: nONR2m4T */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
