95b88946356690efdf65e07b346c2b9c1fc8f0b9
DrittesPhantom <DrittesPhantom@users.noreply.github.com>
2026-07-13 08:35:58 +0000

    deploy: DrittesPhantom/BlazorWebAppTemplate@6edd9c2e59bc8c155adf1f4382713bb837b4e482

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -1,5 +1,5 @@
 self.assetsManifest = {
-  "version": "qT/xfjDn",
+  "version": "DhVOxx9P",
   "assets": [
     {
       "hash": "sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=",
@@ -150,15 +150,15 @@ self.assetsManifest = {
       "url": "_framework/System.Text.RegularExpressions.nz47wj5o6d.wasm"
     },
     {
-      "hash": "sha256-QprDu56hs7mtD81Ym5EJ51cHRPeGZvMUp6bX8hPnqyg=",
-      "url": "_framework/Template.UI.yzzy3681kf.wasm"
+      "hash": "sha256-xDIh6njxw85CZFCmMaSd4IpE7oVLB1FsqdgY/loroIk=",
+      "url": "_framework/Template.UI.0lh37cyukr.wasm"
     },
     {
       "hash": "sha256-A59Tr9HqEhHMu8kU7kruLI9ayw5MRS6Lvc6z7jCbghk=",
       "url": "_framework/blazor.webassembly.js"
     },
     {
-      "hash": "sha256-NsIlUzaz8YJ7cClKMZzv/QB0jXyJ79fVSq7pZTuk1js=",
+      "hash": "sha256-fdl1IsXxbJrXIV14xkSO4L3TyMzGRwi6nRcynGU8pPI=",
       "url": "_framework/dotnet.js"
     },
     {
@@ -166,8 +166,8 @@ self.assetsManifest = {
       "url": "_framework/dotnet.native.2d4rsnc2ac.js"
     },
     {
-      "hash": "sha256-UqbAEPX0PZqsexHwOa1b2L2SQqnfNLL9Ky1KIlpZByM=",
-      "url": "_framework/dotnet.native.mafpcpntgu.wasm"
+      "hash": "sha256-Qi5K1y3aICd+FaE4HPwnSZk2Ui5BRqNt3JFZ0JH4lBI=",
+      "url": "_framework/dotnet.native.6rrnikmng9.wasm"
     },
     {
       "hash": "sha256-7i3usfTrnzC/9qWO4si5Bw4w7D9fUSnSBdhQ47blX2M=",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: qT/xfjDn */
+/* Manifest version: DhVOxx9P */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
